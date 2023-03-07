6f1b9bd22c33ddaf8a5884606238e0a5e2051653
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-03-07 19:04:19 +0200

    add file

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import { nanoid } from 'nanoid';
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import { List } from './ContactList.styled';
 
